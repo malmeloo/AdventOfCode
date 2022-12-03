d4e037c27821a454d12ef20bc02171d8e082553b
Mike A <almeloomike@gmail.com>
2022-12-03 15:01:20 +0100

    [2022-03]

---
+++ 2022/03/challenge.nim
@@ -0,0 +1,42 @@
+import std/[strutils];
+
+
+proc getInput(filename: string): seq[string] =
+  let path: string = rsplit(currentSourcePath(), "/", maxsplit=1)[0]
+  let f = open(path & "/" & filename)
+  defer: f.close()
+
+  result = @[]
+  while true:
+    try:
+      result.add(readLine(f))
+    except EOFError:
+      return
+
+
+proc charScore(character: char): int =
+  if isUpperAscii(character):
+    ord(character) - ord('A') + 27
+  else:
+    ord(character) - ord('a') + 1
+
+
+proc challenge1(inp: seq[string]): int =
+  for sack in inp:
+    for item in sack[0 .. len(sack) div 2 - 1]:
+      if item in sack[len(sack) div 2 .. ^1]:
+        result += charScore(item)
+        break
+
+
+proc challenge2(inp: seq[string]): int =
+  for i in countup(0, len(inp) - 3, 3):
+    for item in inp[i]:
+      if item in inp[i+1] and item in inp[i+2]:
+        result += charScore(item)
+        break;
+
+
+let inp = getInput("input.txt")
+echo(challenge1(inp))
+echo(challenge2(inp))
